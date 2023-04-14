d81a1251e19f89269a68bf038e94782707468a9e
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2023-04-14 09:19:36 +0200

    Use the pulse test in PULSE. (#3)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,28 +4,30 @@ from sklearn.linear_model import LinearRegression
 
 from anchor_regression import LinearAnchorRegression
 from anchor_regression.testing import simulate_iv
-from anchor_regression.utils import anderson_rubin_test
+from anchor_regression.utils import pulse_test
 
 
 @pytest.mark.parametrize("discrete", [True, False])
 @pytest.mark.parametrize("p", [1, 5])
 @pytest.mark.parametrize("dim_y", [1, 2])
-def test_anderson_rubin_test_tsls(discrete, p, dim_y):
+def test_pulse_test_tsls(discrete, p, dim_y):
     X, Y, A = simulate_iv(discrete=discrete, p=p, seed=0, shift=0, dim_y=dim_y)
     Xhat = LinearRegression(fit_intercept=True).fit(A, X).predict(A)
     tsls = LinearRegression(fit_intercept=True).fit(Xhat, Y)
     residuals = Y - tsls.predict(X)
-    p_value, _ = anderson_rubin_test(A, residuals)
-    assert p_value < 1e-3
+    _, p_value = pulse_test(A, residuals)
+    assert p_value > 0.05
 
 
 @pytest.mark.parametrize("discrete", [True, False])
 @pytest.mark.parametrize("p", [1, 5])
 @pytest.mark.parametrize("dim_y", [1, 2])
-def test_anderson_rubin_rest_anchor(discrete, p, dim_y):
+def test_pulse_anchor(discrete, p, dim_y):
     X, Y, A = simulate_iv(discrete=discrete, p=p, seed=0, shift=0, dim_y=dim_y)
     gammas = [0, 1, 2, 4, 8, 16, 32, 64]
     ars = [LinearAnchorRegression(gamma=gamma).fit(X, Y, A) for gamma in gammas]
 
-    statistics = [anderson_rubin_test(A, Y - ar.predict(X))[0] for ar in ars]
+    statistics = [pulse_test(A, Y - ar.predict(X))[0] for ar in ars]
     assert np.all(statistics[:-1] >= statistics[1:])  # AR test should be monotonic
+    p_values = [pulse_test(A, Y - ar.predict(X))[1] for ar in ars]
+    assert np.all(p_values[:-1] <= p_values[1:])
