fb65924eb67a9f2d13e5532c953711b136923830
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2023-07-10 11:36:09 +0200

    Add basis pursuit and spaceIV. (#14)
    * Add basis pursuit and spaceIV.
    
    * Add pylops and pyproximal to reqs.
    
    * Get rid of print statement.

---
+++ anchor_regression/anderson_rubin_basis_pursuit.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pylops
+import pyproximal
+import scipy
+
+from anchor_regression.utils import proj
+
+
+def anderson_rubin_basis_pursuit(Z, X, y, alpha=0.05):
+    """
+    Run basis pursuit on the Anderson-Rubin alpha-confidence set.
+
+    Returns `argmin ||beta||_1` subject to `AR(beta) <= alpha-quantile`.
+    """
+    n, q = Z.shape
+    quantile = scipy.stats.f.ppf(1 - alpha, dfn=n - q, dfd=q)
+
+    Z = Z - Z.mean(axis=0)
+    X = X - X.mean(axis=0)
+    y = y - y.mean()
+
+    X_proj = proj(Z, X)
+    X_orth = X - X_proj
+    y_proj = proj(Z, y)
+    y_orth = y - y_proj
+
+    X_tilde = scipy.linalg.cholesky(X.T @ (X_proj - q / (n - q) * quantile * X_orth))
+    y_tilde = (
+        scipy.linalg.inv(X_tilde.T) @ X.T @ (y_proj - q / (n - q) * quantile * y_orth)
+    )
+    s_tilde = (y_proj - q / (n - q) * quantile * y_orth).T @ X @ scipy.linalg.inv(
+        X_tilde.T @ X_tilde
+    ) @ X.T @ (y_proj - q / (n - q) * quantile * y_orth) - (
+        y_proj - q / (n - q) * quantile * y_orth
+    ).T @ y
+
+    return basis_pursuit(X_tilde, y_tilde, s_tilde)
+
+
+def basis_pursuit(A, y, s, n_iter=1000):
+    """
+    Solve the basis pursuit problem.
+
+    Returns `argmin ||x||_1` subject to `||Ax - y||_2 <= s`.
+    """
+    _, m = A.shape
+
+    Aop = pylops.MatrixMult(A)
+    Aop.explicit = False
+
+    f = pyproximal.L1()
+    g = pyproximal.proximal.EuclideanBall(y, np.sqrt(s))
+
+    L = np.real(pylops.MatrixMult(A.T @ A).eigs(1))[0]
+    tau = 0.99
+    mu = tau / L
+
+    xinv = pyproximal.optimization.primaldual.PrimalDual(
+        f, g, Aop, np.zeros(m), tau, mu, niter=n_iter
+    )
+
+    return xinv
---
+++ anchor_regression/space_iv.py
@@ -0,0 +1,60 @@
+import itertools
+
+import numpy as np
+
+from anchor_regression.linear_model import KClass
+from anchor_regression.tests import anderson_rubin_test
+
+
+class SpaceIV:
+    """
+    Run the space IV algorithm.
+
+    Returns `argmin ||beta||_0` subject to `AR(beta) <= alpha-quantile`.
+
+    Parameters
+    ----------
+    s_max : int, optional
+        Maximum number of variables to consider.
+    alpha : float, optional
+        Confidence level.
+    """
+
+    def __init__(self, s_max=None, alpha=0.05):
+        self.alpha = alpha
+        self.s_max = s_max
+
+    def fit(self, X, y, Z=None):  # noqa D
+        liml = KClass(kappa="liml")
+        if self.s_max is None:
+            self.s_max = min(Z.shape[1], X.shape[1])
+
+        for s in range(1, self.s_max + 1):
+            best_p_value = 0
+            best_S = np.array([])
+            best_coef_ = None
+            best_intercept_ = None
+
+            for S in itertools.combinations(range(X.shape[1]), s):
+                S = np.array(S)
+                if len(S) == 0:
+                    p_val = anderson_rubin_test(Z, y)[1]
+                else:
+                    liml.fit(X[:, S], y, Z=Z)
+                    p_val = anderson_rubin_test(Z, y - liml.predict(X[:, S]))[1]
+
+                if p_val > best_p_value:
+                    best_p_value = p_val
+                    best_S = S
+                    best_coef_ = liml.coef_
+                    best_intercept_ = liml.intercept_
+
+            if best_p_value > self.alpha:
+                self.coef_ = np.zeros(X.shape[1])
+                self.coef_[best_S] = best_coef_
+                self.intercept_ = best_intercept_
+                self.S_ = best_S
+                self.s_ = s
+                break
+
+        return self
