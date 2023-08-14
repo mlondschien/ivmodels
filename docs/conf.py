cee044b835554a006c2fb3d95b5dd3c01cff5eeb
Malte Londschien <malte@londschien.de>
2023-08-14 16:19:43 +0200

    Add basics for sphinx documentation.

---
+++ docs/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = "ivmodels"
+copyright = "2023, Malte Londschien"
+author = "Malte Londschien"
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinxcontrib.apidoc",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "sphinx_rtd_theme",
+]
+
+apidoc_module_dir = "../ivmodels"
+apidoc_output_dir = "api"
+apidoc_extra_args = ["--implicit-namespaces"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+This is `ivmodels`' documentation
+===========================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   API Reference <api/modules>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
