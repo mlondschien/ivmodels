44f5d821d541ff324c0ed9e9eb5f1eaee129e827
Malte Londschien <malte@londschien.de>
2023-08-24 16:12:52 +0200

    No _static.

--- docs/conf.py
+++ docs/conf.py
@@ -49,4 +49,3 @@ apidoc_extra_args = ["--implicit-namespaces"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
