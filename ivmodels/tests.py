199c5c74694ef93a0b69c686d0f15f8bf9a2fe11
Malte Londschien <malte@londschien.de>
2023-10-25 19:58:35 +0200

    Add LM test. Make LR test linear.

--- ivmodels/tests.py
+++ ivmodels/tests.py
@@ -134,7 +134,7 @@ def pulse_test(Z, X, y, beta):
     return statistic, p_value
 
 
-def wald_test(Z, X, y, beta, estimator="tsls"):
+def wald_test(Z, X, y, beta, W=None, estimator="tsls"):
     """
     Test based on asymptotic normality of the TSLS (or LIML) estimator.
 
@@ -196,23 +196,31 @@ def wald_test(Z, X, y, beta, estimator="tsls"):
     return statistic, p_value
 
 
-def anderson_rubin_test(Z, X, y, beta):
+def anderson_rubin_test(Z, X, y, beta, W=None):
     """
     Perform the Anderson Rubin test :cite:p:`anderson1949estimation`.
 
     Test the null hypothesis that the residuals are uncorrelated with the instruments.
-    The test statistic is defined as
+    If `W` is `None`, the test statistic is defined as
 
     .. math:: AR := \\frac{n - q}{q} \\frac{\\| P_Z (y - X \\beta) \\|_2^2}{\\| M_Z  (y - X \\beta) \\|_2^2},
 
     where :math:`P_Z` is the projection matrix onto the column space of :math:`Z` and
     :math:`M_Z = \\mathrm{Id} - P_Z`.
 
-    Under the null and normally distributed errors, the test statistic is distributed as
+    Under the null and normally distributed errors, this test statistic is distributed as
     :math:`F_{q, n - q}``, where :math:`q` is the number of instruments and :math:`n` is
     the number of observations. The statistic is asymptotically distributed as
     :math:`\\chi^2(q) / q` under the null and non-normally distributed errors, even for
     weak instruments.
+
+    If `W` is not `None`, the test statistic is defined as
+
+    .. math:: AR := \\max_\\gamma \\frac{n - q}{q - r} \\frac{\\| P_Z (y - X \\beta - W \\gamma) \\|_2^2}{\\| M_Z  (y - X \\beta - W \\gamma) \\|_2^2},
+
+    Under the null, this test statistic is asymptotically distributed as
+    :math:`\\chi^2(q - r) / (q - r)`, where `r = W.shape[1]`
+    :cite:p:`guggenberger2012asymptotic`.
 
     Parameters
     ----------
@@ -224,15 +232,17 @@ def anderson_rubin_test(Z, X, y, beta):
         Outcomes.
     beta: np.ndarray of dimension (p,)
         Coefficients to test.
+    W: np.ndarray of dimension (n, r) or None
+        Endogenous regressors not of interest.
 
     Returns
     -------
     statistic: float
         The test statistic :math:`AR`.
     p_value: float
-        The p-value of the test. Equal to :math:`1 - F_{F_{q, n - q}}(AR)`, where
-        :math:`F_{F_{q, n - q}}` is the cumulative distribution function of the
-        :math:`F_{q, n - q}` distribution.
+        The p-value of the test. Equal to :math:`1 - F_{F_{q - r, n - q}}(AR)`, where
+        :math:`F_{F_{q - r, n - q}}` is the cumulative distribution function of the
+        :math:`F_{q - r, n - q}` distribution and `r = 0` if `W` is `None`.
 
     Raises
     ------
@@ -245,36 +255,58 @@ def anderson_rubin_test(Z, X, y, beta):
        :filter: False
 
        anderson1949estimation
+       guggenberger2012asymptotic
     """
-    Z, X, y, _, beta = _check_test_inputs(Z, X, y, beta=beta)
+    Z, X, y, W, beta = _check_test_inputs(Z, X, y, W=W, beta=beta)
     n, q = Z.shape
 
-    residuals = y - X @ beta
+    if W is None:
+        residuals = y - X @ beta
+        proj_residuals = proj(Z, residuals)
+        ar = (
+            np.square(proj_residuals).sum()
+            / np.square(residuals - proj_residuals).sum()
+        )
+        ar *= (n - q) / q
+        r = 0
+    else:
+        liml = KClass(kappa="liml").fit(X=W, y=y - X @ beta, Z=Z)
+        ar = (liml.kappa_ - 1) * (n - q) / q
+        r = W.shape[1]
 
-    proj_residuals = proj(Z, residuals)
-    ar = np.square(proj_residuals).sum() / np.square(residuals - proj_residuals).sum()
-    ar *= (n - q) / q
-
-    p_value = 1 - scipy.stats.f.cdf(ar, dfn=q, dfd=n - q)
+    p_value = 1 - scipy.stats.f.cdf(ar, dfn=q - r, dfd=n - q)
 
     return ar, p_value
 
 
-def likelihood_ratio_test(Z, X, y, beta):
+def likelihood_ratio_test(Z, X, y, beta, W=None):
     """
     Perform the likelihood ratio test for ``beta``.
 
-    The test statistic is defined as
+    If ``W = None``, the test statistic is defined as
 
     .. math::
 
-       \\mathrm{LR} &:= n \\log(1 + \\frac{(y - X \\beta)^T P_Z (y - X \\beta)}{(y - X \\beta)^T M_Z (y - X \\beta)}) - n \\log(1 + \\frac{(y - X \\beta_\\mathrm{LIML})^T P_Z (y - X \\beta_\\mathrm{LIML})}{(y - X \\beta_\\mathrm{LIML})^T M_Z (y - X \\beta_\\mathrm{LIML})}) \\\\
-       &= n \\log(1 + \\frac{q}{n-q}\\mathrm{AR}(\\beta)) - n \\log(1 + \\frac{q}{n-q}\\mathrm{AR}(\\beta_\\mathrm{LIML})),
+       \\mathrm{LR(\\beta)} &:= (n - q) \\frac{ \\| P_Z (y - X \\beta) \\|_2^2}{ \\| M_Z (y - X \\beta) \\|_2^2} - (n - q) \\frac{ \\| P_Z (y - X \\hat\\beta_\\mathrm{LIML}) \\|_2^2 }{ \\| M_Z (y - X \\hat\\beta_\\mathrm{LIML}) \\|_2^2 } \\\\
+       &= q \\ \\mathrm{AR}(\\beta)) - q \\ \\mathrm{AR}(\\hat\\beta_\\mathrm{LIML}),
 
     where :math:`P_Z` is the projection matrix onto the column space of :math:`Z`,
-    :math:`M_Z = \\mathrm{Id} - P_Z`, and :math:`\\beta_\\mathrm{LIML}` is the LIML
-    estimator of :math:`\\beta`, minimizing :math:`\\mathrm{AR}(\\beta)` at
-    :math:`\\mathrm{AR}(\\beta_\\mathrm{LIML}) = \\frac{n - q}{q} (\\hat\\kappa_\\mathrm{LIML} - 1)`.
+    :math:`M_Z = \\mathrm{Id} - P_Z`, and :math:`\\hat\\beta_\\mathrm{LIML}` is the LIML
+    estimator of :math:`\\beta`, minimizing the Anderson-Rubin test statistic
+    :math:`\\mathrm{AR}(\\beta)` (see :py:func:`ivmodels.tests.anderson_rubin_test`) at
+    :math:`\\mathrm{AR}(\\hat\\beta_\\mathrm{LIML}) = \\frac{n - q}{q} (\\hat\\kappa_\\mathrm{LIML} - 1)`.
+
+    If ``W != None``, the test statistic is defined as
+
+    .. math::
+
+       \\mathrm{LR(\\beta)} := (n - q) \\frac{ \\|P_Z (y - X \\beta - W \\hat\\gamma_\\mathrm{LIML}) \\|^2_2 }{\\| M_Z (y - X \\beta - W \\hat\\gamma_\\mathrm{LIML}) \\|_2^2 } - (n - q) \\frac{\\| P_Z (y - (X \\ W) \\hat\\delta_\\mathrm{LIML}) \\|_2^2 }{ \\| M_Z (y - (X \\ W) \\hat\\delta_\\mathrm{LIML}) \\|_2^2}
+
+    where :math:`\\gamma_\\mathrm{LIML}` is the LIML estimator (see
+    :py:class:`ivmodels.kclass.KClass`) using instruments :math:`Z`, endogenous
+    covariates :math:`W`, and outcomes :math:`y - X \\beta` and
+    :math:`\\hat\\delta_\\mathrm{LIML}` is the LIML estimator
+    using instruments :math:`Z`, endogenous covariates :math:`X \\ W`, and outcomes :math:`y`.
 
     Under the null and given strong instruments, the test statistic is asymptotically
     distributed as :math:`\\chi^2(p)`, where :math:`p` is the number of regressors.
@@ -289,6 +321,8 @@ def likelihood_ratio_test(Z, X, y, beta):
         Outcomes.
     beta: np.ndarray of dimension (p,)
         Coefficients to test.
+    W: np.ndarray of dimension (n, r) or None
+        Endogenous regressors not of interest.
 
     Returns
     -------
@@ -304,24 +338,82 @@ def likelihood_ratio_test(Z, X, y, beta):
     ValueError:
         If the dimensions of the inputs are incorrect.
     """
-    Z, X, y, _, beta = _check_test_inputs(Z, X, y, beta=beta)
+    Z, X, y, W, beta = _check_test_inputs(Z, X, y, W=W, beta=beta)
 
     n, p = X.shape
+    q = Z.shape[1]
+
+    if W is None:
+        W = np.zeros((n, 0))
 
     X_proj = proj(Z, X)
     y_proj = proj(Z, y)
-    Xy_proj = np.concatenate([X_proj, y_proj.reshape(-1, 1)], axis=1)
-    Xy = np.concatenate([X, y.reshape(-1, 1)], axis=1)
+    W_proj = proj(Z, W)
 
-    W = np.linalg.solve(Xy.T @ Xy, Xy_proj.T @ Xy)
-    eta_liml = min(np.abs(scipy.linalg.eigvals(W)))
-    kappa_liml = eta_liml / (1 - eta_liml)
+    XWy = np.concatenate([X, W, y.reshape(-1, 1)], axis=1)
+    XWy_proj = np.concatenate([X_proj, W_proj, y_proj.reshape(-1, 1)], axis=1)
 
-    residuals = y - X @ beta
-    proj_residuals = y_proj - X_proj @ beta
+    matrix = np.linalg.solve(XWy.T @ (XWy - XWy_proj), XWy_proj.T @ XWy)
+    ar_min = (n - q) * min(np.abs(scipy.linalg.eigvals(matrix)))
 
-    ar = np.square(proj_residuals).sum() / np.square(residuals - proj_residuals).sum()
-    statistic = n * (np.log1p(ar) - np.log1p(kappa_liml))
+    if W.shape[1] == 0:
+        statistic = (n - q) * np.linalg.norm(
+            y_proj - X_proj @ beta
+        ) ** 2 / np.linalg.norm((y - y_proj) - (X - X_proj) @ beta) ** 2 - ar_min
+    else:
+        Wy = np.concatenate([W, y - X @ beta], axis=1)
+        Wy_proj = np.concatenate([W_proj, y_proj - X_proj @ beta], axis=1)
+        matrix = np.linalg.solve(Wy.T @ (Wy - Wy_proj), Wy_proj.T @ Wy)
+        statistic = (n - q) * min(np.abs(scipy.linalg.eigvals(matrix))) - ar_min
+
+    p_value = 1 - scipy.stats.chi2.cdf(statistic, df=p)
+
+    return statistic, p_value
+
+
+def lagrange_multiplier_test(Z, X, y, beta):
+    """
+    Perform the Lagrange multiplier test for ``beta`` :cite:p:`kleibergen2002pivotal`.
+
+    Test the null hypothesis that the residuals are uncorrelated with the instruments.
+    Let
+
+    .. math:: \\tilde X(\\beta) := X - (y - X \\beta) \\frac{(y - X \\beta) M_Z X}{(y - X \\beta) M_Z (y - X \\beta)}.
+
+    The test statistic is
+
+    .. math:: LM := (n - q) \\frac{\\| P_{P_Z \\tilde X(\\beta)} (y - X \\beta) \\|_2^2}{\\| M_Z  (y - X \\beta) \\|_2^2},
+
+    This test statistic is asymptotically distributed as :math:`\\chi^2(p)` under the
+    null, even if the instruments are weak :cite:p:`kleibergen2002pivotal`.
+
+    Parameters
+    ----------
+    Z: np.ndarray of dimension (n, q)
+        Instruments.
+    X: np.ndarray of dimension (n, p)
+        Regressors.
+    y: np.ndarray of dimension (n,)
+        Outcomes.
+    beta: np.ndarray of dimension (p,)
+        Coefficients to test.
+
+    """
+    Z, X, y, _, beta = _check_test_inputs(Z, X, y, beta=beta)
+    n, q = Z.shape
+    p = X.shape[1]
+
+    residuals = (y - X @ beta).reshape(-1, 1)
+    proj_residuals = proj(Z, residuals)
+    orth_residuals = residuals - proj_residuals
+
+    # X - (y - X beta) * (y - X beta)^T M_Z X / (y - X beta)^T M_Z (y - X beta)
+    X_tilde = X - residuals @ (orth_residuals.T @ X) / (residuals.T @ orth_residuals)
+    proj_X_tilde = proj(Z, X_tilde)
+    X_tilde_proj_residuals = proj(proj_X_tilde, residuals)
+    # (y - X beta) P_{P_Z X_tilde} (y - X beta) / (y - X_beta) M_Z (y - X beta)
+    statistic = np.square(X_tilde_proj_residuals).sum() / (residuals.T @ orth_residuals)
+    statistic *= n - q
 
     p_value = 1 - scipy.stats.chi2.cdf(statistic, df=p)
 
@@ -449,8 +541,8 @@ def inverse_likelihood_ratio_test(Z, X, y, alpha=0.05):
         raise ValueError("alpha must be in (0, 1).")
 
     Z, X, y, _, _ = _check_test_inputs(Z, X, y)
-
     n, p = X.shape
+    q = Z.shape[1]
 
     Z = Z - Z.mean(axis=0)
     X = X - X.mean(axis=0)
@@ -464,15 +556,14 @@ def inverse_likelihood_ratio_test(Z, X, y, alpha=0.05):
     Xy_proj = np.concatenate([X_proj, y_proj.reshape(-1, 1)], axis=1)
     Xy = np.concatenate([X, y.reshape(-1, 1)], axis=1)
 
-    W = np.linalg.solve(Xy.T @ Xy, Xy.T @ Xy_proj)
-    eta_liml = min(np.linalg.eigvals(W))
-    kappa_liml = eta_liml / (1 - eta_liml)
+    W = np.linalg.solve(Xy.T @ (Xy - Xy_proj), Xy.T @ Xy_proj)
+    kappa_liml = min(np.abs(np.linalg.eigvals(W)))
 
-    quantile = np.exp(scipy.stats.chi2.ppf(1 - alpha, df=p) / n) * (1 + kappa_liml) - 1
+    quantile = scipy.stats.chi2.ppf(1 - alpha, df=p) + (n - q) * kappa_liml
 
-    A = X.T @ (X_proj - quantile * X_orth)
-    b = -2 * (X_proj - quantile * X_orth).T @ y
-    c = y.T @ (y_proj - quantile * y_orth)
+    A = X.T @ (X_proj - 1 / (n - q) * quantile * X_orth)
+    b = -2 * (X_proj - 1 / (n - q) * quantile * X_orth).T @ y
+    c = y.T @ (y_proj - 1 / (n - q) * quantile * y_orth)
 
     if isinstance(c, np.ndarray):
         c = c.item()
--- tests/test_tests.py
+++ tests/test_tests.py
@@ -10,6 +10,7 @@ from ivmodels.tests import (
     inverse_likelihood_ratio_test,
     inverse_pulse_test,
     inverse_wald_test,
+    lagrange_multiplier_test,
     likelihood_ratio_test,
     pulse_test,
     wald_test,
@@ -18,15 +19,35 @@ from ivmodels.tests import (
 TEST_PAIRS = [
     (anderson_rubin_test, inverse_anderson_rubin_test),
     (pulse_test, inverse_pulse_test),
-    (wald_test, inverse_wald_test),
+    (lagrange_multiplier_test, None),
     (likelihood_ratio_test, inverse_likelihood_ratio_test),
+    (wald_test, inverse_wald_test),
 ]
+
+# def test_projected_test():
+#     n=1000
+#     p = 2
+#     q = 3
+#     u = 3
+
+#     Z, X, y = simulate_gaussian_iv(n, p, q, u, seed=0)
+
+#     Z = Z - Z.mean(axis=0)
+#     X = X - X.mean(axis=0)
+#     y = y.flatten() - y.mean()
+
+#     W = np.zeros((n, 0))
+
+
+#     res1=projected_anderson_rubin_test(Z, X, W, y, np.zeros((p, 1)))
+#     res2=anderson_rubin_test(Z, X, y,  np.zeros((p, 1)))
+#     breakpoint()
 
 
 @pytest.mark.parametrize("test", [pair[0] for pair in TEST_PAIRS])
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
 def test_test_size(test, n, p, q, u):
-
+    """Test that the test size is close to the nominal level."""
     n_seeds = 200
     p_values = np.zeros(n_seeds)
 
@@ -50,10 +71,41 @@ def test_test_size(test, n, p, q, u):
     assert np.mean(p_values < 0.05) < 0.07  # 4 stds above 0.05 for n_seeds = 100
 
 
-@pytest.mark.parametrize("test, inverse_test", TEST_PAIRS)
+# The wald and likelihood ratio tests are not valid for weak instruments
+@pytest.mark.parametrize("test", [pair[0] for pair in TEST_PAIRS[:-2]])
+@pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (1000, 2, 5, 2)])
+def test_test_size_weak_ivs(test, n, p, q, u):
+    """Test that the test size is close to the nominal level for weak instruments."""
+    n_seeds = 1000
+    p_values = np.zeros(n_seeds)
+
+    for seed in range(n_seeds):
+        rng = np.random.RandomState(seed)
+
+        delta = rng.normal(0, 1, (u, p))
+        gamma = rng.normal(0, 1, (u, 1))
+
+        beta = rng.normal(0, 0.1, (p, 1))
+        Pi = rng.normal(0, 1, (q, p)) / np.sqrt(n)
+
+        U = rng.normal(0, 1, (n, u))
+
+        Z = rng.normal(0, 1, (n, q))
+        X = Z @ Pi + U @ delta + rng.normal(0, 1, (n, p))
+        y = X @ beta + U @ gamma + rng.normal(0, 1, (n, 1))
+
+        _, p_values[seed] = test(Z, X, y, beta)
+
+    assert np.mean(p_values < 0.05) < 0.07  # 4 stds above 0.05 for n_seeds = 100
+
+
+@pytest.mark.parametrize(
+    "test, inverse_test", [(p[0], p[1]) for p in TEST_PAIRS if p[1] is not None]
+)
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
 @pytest.mark.parametrize("p_value", [0.1, 0.01])
 def test_test_round_trip(test, inverse_test, n, p, q, u, p_value):
+    """A test's p-value at the confidence set's boundary equals the nominal level."""
     Z, X, y = simulate_gaussian_iv(n, p, q, u, seed=0)
 
     Z = Z - Z.mean(axis=0)
@@ -76,6 +128,7 @@ def test_test_round_trip(test, inverse_test, n, p, q, u, p_value):
 @pytest.mark.parametrize("kappa", ["liml", "tsls"])
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
 def test_p_value_of_estimator(test, kappa, n, p, q, u):
+    """The estimated coefficient should be in the confidence set with 95% coverage."""
     Z, X, y = simulate_gaussian_iv(n, p, q, u)
     estimator = KClass(kappa=kappa).fit(X, y.flatten(), Z=Z)
     p_value = test(Z, X, y, estimator.coef_)[1]
@@ -84,7 +137,8 @@ def test_p_value_of_estimator(test, kappa, n, p, q, u):
 
 @pytest.mark.parametrize("test", [anderson_rubin_test, pulse_test])
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
-def test_ar_test_monotinic_in_kappa(test, n, p, q, u):
+def test_ar_test_monotonic_in_kappa(test, n, p, q, u):
+    """AR(beta(kappa)) should be decreasing in kappa increasing towards 1."""
     A, X, Y = simulate_gaussian_iv(n, p, q, u)
     Y = Y.flatten()
     kappas = np.linspace(0, 1, 10)
@@ -97,8 +151,11 @@ def test_ar_test_monotinic_in_kappa(test, n, p, q, u):
 
 
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
-@pytest.mark.parametrize("inverse_test", [pair[1] for pair in TEST_PAIRS])
+@pytest.mark.parametrize(
+    "inverse_test", [pair[1] for pair in TEST_PAIRS if pair[1] is not None]
+)
 def test_inverse_test_sorted(inverse_test, n, p, q, u):
+    """The volume of confidence sets should be increasing in the p-value."""
     Z, X, y = simulate_gaussian_iv(n, p, q, u, seed=0)
 
     p_values = [0.5, 0.2, 0.1, 0.05]
@@ -109,32 +166,32 @@ def test_inverse_test_sorted(inverse_test, n, p, q, u):
     assert volumes[0] <= volumes[1] <= volumes[2] <= volumes[3]
 
 
-@pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
-@pytest.mark.parametrize("seed", [0, 1])
-def test_inverse_anderson_rubin_below_above(n, p, q, u, seed):
-    rng = np.random.RandomState(seed)
+# @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
+# @pytest.mark.parametrize("seed", [0, 1])
+# def test_inverse_anderson_rubin_below_above(n, p, q, u, seed):
+#     rng = np.random.RandomState(seed)
 
-    delta = rng.normal(0, 1, (u, p))
-    gamma = rng.normal(0, 1, (u, 1))
+#     delta = rng.normal(0, 1, (u, p))
+#     gamma = rng.normal(0, 1, (u, 1))
 
-    beta = rng.normal(0, 0.1, (p, 1))
-    Pi = rng.normal(0, 1, (q, p))
+#     beta = rng.normal(0, 0.1, (p, 1))
+#     Pi = rng.normal(0, 1, (q, p))
 
-    U = rng.normal(0, 1, (n, u))
+#     U = rng.normal(0, 1, (n, u))
 
-    Z = rng.normal(0, 1, (n, q))
-    X = Z @ Pi + U @ delta + rng.normal(0, 1, (n, p))
-    y = X @ beta + U @ gamma + rng.normal(0, 1, (n, 1))
+#     Z = rng.normal(0, 1, (n, q))
+#     X = Z @ Pi + U @ delta + rng.normal(0, 1, (n, p))
+#     y = X @ beta + U @ gamma + rng.normal(0, 1, (n, 1))
 
-    X = X - X.mean(axis=0)
-    y = y - y.mean()
+#     X = X - X.mean(axis=0)
+#     y = y - y.mean()
 
-    beta_hat = rng.normal(0, 0.1, (p, 1))
-    _, p_value = anderson_rubin_test(Z, X, y.flatten(), beta_hat)
-    below = inverse_anderson_rubin_test(Z, X, y, p_value * 0.999)
-    above = inverse_anderson_rubin_test(Z, X, y, p_value + (1 - p_value) * 0.001)
-    assert below(beta_hat.reshape(1, -1)) < 0
-    assert above(beta_hat.reshape(1, -1)) > 0
+#     beta_hat = rng.normal(0, 0.1, (p, 1))
+#     _, p_value = anderson_rubin_test(Z, X, y.flatten(), beta_hat)
+#     below = inverse_anderson_rubin_test(Z, X, y, p_value * 0.999)
+#     above = inverse_anderson_rubin_test(Z, X, y, p_value + (1 - p_value) * 0.001)
+#     assert below(beta_hat.reshape(1, -1)) < 0
+#     assert above(beta_hat.reshape(1, -1)) > 0
 
 
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2), (100, 2, 1, 2)])
