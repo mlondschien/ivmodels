Fix threshold calculation.

ivmodels/tests.py
@@ -479,9 +479,6 @@ def conditional_likelihood_ratio_test(Z, X, y, beta, W=None):  # noqa D
     p = X.shape[1]
     r = W.shape[1] if W is not None else 0
 
-    if q == p:
-        return anderson_rubin_test(Z, X, y, beta, W=W)
-
     if W is None:
         XW = X
     else:
@@ -499,13 +496,10 @@ def conditional_likelihood_ratio_test(Z, X, y, beta, W=None):  # noqa D
     XWt_proj = XW_proj - np.outer(residuals_proj, Sigma)
     XWt_orth = XWt - XWt_proj
     mat_XWt = np.linalg.solve(XWt_orth.T @ XWt_orth, XWt_proj.T @ XWt_proj)
-    s_min = min(np.real(np.linalg.eigvals(mat_XWt)))
+    s_min = min(np.real(np.linalg.eigvals(mat_XWt))) * (n - q)
 
     # TODO: This can be done with efficient rank-1 updates.
-    XWy = np.concatenate([XW, y.reshape(-1, 1)], axis=1)
-    XWy_proj = np.concatenate([XW_proj, y_proj.reshape(-1, 1)], axis=1)
-    mat_XWy = np.linalg.solve((XWy - XWy_proj).T @ XWy, XWy_proj.T @ XWy_proj)
-    ar_min = min(np.real(np.linalg.eigvals(mat_XWy)))
+    ar_min = KClass.ar_min(X=XW, y=y, Z=Z)
 
     if r == 0:
         ar = residuals_proj.T @ residuals_proj / (residuals_orth.T @ residuals_orth)
@@ -517,9 +511,13 @@ def conditional_likelihood_ratio_test(Z, X, y, beta, W=None):  # noqa D
 
     chi2p = scipy.stats.chi2.rvs(size=1000, df=p, random_state=0)
     chi2r = scipy.stats.chi2.rvs(size=1000, df=r, random_state=1) if r > 0 else 0
-    chi2q = scipy.stats.chi2.rvs(size=1000, df=q - p, random_state=2)
-    D = np.sqrt((chi2q + chi2p + s_min) ** 2 - 4 * chi2q * s_min)
-    Q = 1 / 2 * (chi2q - chi2r + chi2p + s_min + D)
+    chi2q = (
+        scipy.stats.chi2.rvs(size=1000, df=q - p - r, random_state=2)
+        if q - p - r > 0
+        else 0
+    )
+    D = np.sqrt((chi2q + chi2p + chi2r + s_min) ** 2 - 4 * (chi2q) * s_min)
+    Q = 1 / 2 * (chi2q + chi2p - chi2r - s_min + D)
     p_value = np.mean(Q > statistic)
 
     return statistic, p_value

tests/test_tests.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import numpy as np
 import pytest
 import scipy
@@ -18,6 +20,9 @@ from ivmodels.tests import (
     wald_test,
 )
 
+liml_wald_test = partial(wald_test, estimator="liml")
+liml_inverse_wald_test = partial(inverse_wald_test, estimator="liml")
+
 TEST_PAIRS = [
     (conditional_likelihood_ratio_test, None),
     (pulse_test, inverse_pulse_test),
@@ -34,6 +39,7 @@ TEST_PAIRS = [
     [
         anderson_rubin_test,
         wald_test,
+        liml_wald_test,
         likelihood_ratio_test,
         conditional_likelihood_ratio_test,
     ],
@@ -66,6 +72,7 @@ def test_subvector_test_equal_to_original(test, n, p, r, q, u):
     [
         anderson_rubin_test,
         wald_test,
+        liml_wald_test,
         likelihood_ratio_test,
         conditional_likelihood_ratio_test,
     ],
@@ -160,6 +167,7 @@ def test_subvector_test_size_weak_instruments(test, n, q):
         lagrange_multiplier_test,
         anderson_rubin_test,
         wald_test,
+        liml_wald_test,
         likelihood_ratio_test,
         conditional_likelihood_ratio_test,
     ],
@@ -190,20 +198,20 @@ def test_test_size(test, n, p, q, u):
     assert np.mean(p_values < 0.05) < 0.07  # 4 stds above 0.05 for n_seeds = 100
 
 
-# The wald and likelihood ratio tests are not valid for weak instruments
+# The wald, and likelihood ratio tests are not valid for weak instruments
 @pytest.mark.parametrize(
     "test",
     [
-        pulse_test,
         lagrange_multiplier_test,
         anderson_rubin_test,
+        pulse_test,
         conditional_likelihood_ratio_test,
     ],
 )
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (1000, 2, 5, 2)])
 def test_test_size_weak_ivs(test, n, p, q, u):
     """Test that the test size is close to the nominal level for weak instruments."""
-    n_seeds = 1000
+    n_seeds = 200
     p_values = np.zeros(n_seeds)
 
     for seed in range(n_seeds):
@@ -232,6 +240,7 @@ def test_test_size_weak_ivs(test, n, p, q, u):
         (pulse_test, inverse_pulse_test),
         (anderson_rubin_test, inverse_anderson_rubin_test),
         (wald_test, inverse_wald_test),
+        (liml_wald_test, liml_inverse_wald_test),
         (likelihood_ratio_test, inverse_likelihood_ratio_test),
     ],
 )
@@ -261,6 +270,7 @@ def test_test_round_trip(test, inverse_test, n, p, q, u, p_value):
     "test, inverse_test",
     [
         (wald_test, inverse_wald_test),
+        (liml_wald_test, liml_inverse_wald_test),
         (anderson_rubin_test, inverse_anderson_rubin_test),
         (likelihood_ratio_test, inverse_likelihood_ratio_test),
     ],
@@ -299,6 +309,7 @@ def test_subvector_round_trip(test, inverse_test, n, p, q, u, r, p_value):
         lagrange_multiplier_test,
         anderson_rubin_test,
         wald_test,
+        liml_wald_test,
         likelihood_ratio_test,
         conditional_likelihood_ratio_test,
     ],
@@ -316,13 +327,13 @@ def test_p_value_of_estimator(test, kappa, n, p, q, u):
 @pytest.mark.parametrize("test", [anderson_rubin_test, pulse_test])
 @pytest.mark.parametrize("n, p, q, u", [(100, 2, 2, 1), (100, 2, 5, 2)])
 def test_ar_test_monotonic_in_kappa(test, n, p, q, u):
-    """AR(beta(kappa)) should be decreasing in kappa increasing towards 1."""
-    A, X, Y = simulate_gaussian_iv(n, p, q, u)
+    """AR(beta(kappa)) should be decreasing in kappa increasing towards kappa."""
+    Z, X, Y = simulate_gaussian_iv(n, p, q, u)
     Y = Y.flatten()
-    kappas = np.linspace(0, 1, 10)
-    models = [KClass(kappa=kappa).fit(X, Y, Z=A) for kappa in kappas]
-    statistics = [test(A, X, Y, model.coef_)[0] for model in models]
-    p_values = [test(A, X, Y, model.coef_)[1] for model in models]
+    kappas = np.linspace(0, KClass.ar_min(X, Y, Z) + 1, 10)
+    models = [KClass(kappa=kappa).fit(X, Y, Z=Z) for kappa in kappas]
+    statistics = [test(Z, X, Y, model.coef_)[0] for model in models]
+    p_values = [test(Z, X, Y, model.coef_)[1] for model in models]
 
     assert np.all(statistics[:-1] >= statistics[1:])
     assert np.all(p_values[:-1] <= p_values[1:])
@@ -335,6 +346,7 @@ def test_ar_test_monotonic_in_kappa(test, n, p, q, u):
         inverse_pulse_test,
         inverse_anderson_rubin_test,
         inverse_wald_test,
+        liml_inverse_wald_test,
         inverse_likelihood_ratio_test,
     ],
 )