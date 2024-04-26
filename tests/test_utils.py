261596907f92a1254f7c5404bab348ed59dd0959
Malte Londschien <malte@londschien.de>
2024-04-26 13:39:51 +0200

    Test oproj

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,19 @@
 import numpy as np
 
-from ivmodels.utils import proj
+from ivmodels.utils import oproj, proj
 
 
 def test_proj():
     rng = np.random.RandomState(0)
     X = rng.normal(0, 1, (100, 2))
     Z = rng.normal(0, 1, (100, 5))
-    Z = Z - Z.mean(axis=0)
 
     assert np.allclose(proj(Z, X), Z @ np.linalg.inv(Z.T @ Z) @ Z.T @ X)
+
+
+def test_oproj():
+    rng = np.random.RandomState(0)
+    X = rng.normal(0, 1, (100, 2))
+    Z = rng.normal(0, 1, (100, 5))
+
+    assert np.allclose(X - proj(Z, X), oproj(Z, X))
