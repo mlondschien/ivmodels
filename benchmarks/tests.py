9768e8da123b8f009860d2d045c73b6dbcced1f9
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2024-06-04 10:27:06 +0200

    Add asv benchmarks (#68)
    * Add asv benchmarks.
    
    * Add CI (?)
    
    * Fix CI (1)
    
    * Fix CI (2)
    
    * Fix CI (3)
    
    * Fix CI (3)
    
    * Increase max possible iterations for convergence in power_series method for CLR
    
    * Fewer params
    
    * Fix CI (5)
    
    * Fix CI (6)
    
    * Fix CI (7)
    
    * asv continuous
    
    * Don't publish html results?
    
    * continuous
    
    * Move asv machine --yes.
    
    * Update env.yml.
    
    * Use setup-micromamba.
    
    * with
    
    * just bash.
    
    * Get rid of 'pythons' key in asv.config.
    
    * Force mamba reinstall.
    
    * No -e
    
    * Add mamba to path.
    
    * Comment mamba reinstall.
    
    * Get rid of python==same
    
    * Conda instead of mamba?
    
    * Add run benchmarks back in
    
    * -y
    
    * Branches: ['main'] back in
    
    * Move everything up one folder.
    
    * Move rest.
    
    * Install repo.
    
    * 'branches': []?
    
    * 'branches': ['HEAD']?
    
    * Don't cd benchmarks.
    
    * python=same for asv continuous
    
    * 'branches': ['origin/main']?
    
    * try custom plugin like iris
    
    * Just try old mamba?
    
    * Just asv continuous.
    
    * Downgrade conda
    
    * Fix command.
    
    * -v
    
    * Remove asv ci.
    
    * Remove line.

---
+++ benchmarks/tests.py
@@ -0,0 +1,71 @@
+from asv_runner.benchmarks.mark import skip_for_params
+
+from ivmodels.simulate import simulate_gaussian_iv
+from ivmodels.tests import (
+    anderson_rubin_test,
+    conditional_likelihood_ratio_test,
+    lagrange_multiplier_test,
+    likelihood_ratio_test,
+    rank_test,
+    wald_test,
+)
+
+
+class Tests:
+    # dims is (k, mx, mw, mc)
+    params = [
+        [100, 1000, 10000],
+        [(1, 1, 0, 0), (4, 2, 0, 0), (10, 1, 4, 0), (4, 2, 2, 2)],
+    ]
+    param_names = ["n", "dims"]
+
+    def setup(self, n, dims):
+
+        k, mx, mw, mc = dims
+        Z, X, y, C, W, beta = simulate_gaussian_iv(
+            n=n, mx=mx, k=k, u=mx, mc=mc, mw=mw, return_beta=True
+        )
+
+        self.data = {"Z": Z, "X": X, "y": y, "C": C, "W": W, "beta": beta}
+
+    def time_anderson_rubin_test(self, n, dims):
+        _, _ = anderson_rubin_test(**self.data)
+
+    @skip_for_params(
+        [
+            (100, (1, 1, 0, 0)),
+            (100, (4, 2, 0, 0)),
+            (100, (4, 2, 0, 2)),
+            (1000, (1, 1, 0, 0)),
+            (1000, (4, 2, 0, 0)),
+            (1000, (4, 2, 0, 2)),
+            (10000, (1, 1, 0, 0)),
+            (10000, (4, 2, 0, 0)),
+            (10000, (4, 2, 0, 2)),
+        ]
+    )
+    def time_anderson_rubin_test_guggenberger19(self, n, dims):
+        _, _ = anderson_rubin_test(**self.data, critical_values="guggenberger19")
+
+    def time_lagrange_multiplier_test(self, n, dims):
+        _, _ = lagrange_multiplier_test(**self.data)
+
+    def time_likelihood_ratio_test(self, n, dims):
+        _, _ = likelihood_ratio_test(**self.data)
+
+    def time_conditional_likelihood_ratio_test_numerical_integration(self, n, dims):
+        _, _ = conditional_likelihood_ratio_test(
+            **self.data, method="numerical_integration"
+        )
+
+    def time_conditional_likelihood_ratio_test_power_series(self, n, dims):
+        _, _ = conditional_likelihood_ratio_test(**self.data, method="power_series")
+
+    def time_wald_test_tsls(self, n, dims):
+        _, _ = wald_test(**self.data, estimator="tsls")
+
+    def time_wald_test_liml(self, n, dims):
+        _, _ = wald_test(**self.data, estimator="liml")
+
+    def test_rank_test(self, n, dims):
+        _, _ = rank_test(Z=self.data["Z"], X=self.data["X"])
