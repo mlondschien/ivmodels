2c7335b94e22c8a565387d9ed8f35e0bfd6237d8
Malte Londschien <malte@londschien.de>
2024-07-07 16:21:42 +0200

    empty and finite as well.

--- tests/test_confidence_set.py
+++ tests/test_confidence_set.py
@@ -5,14 +5,17 @@ from ivmodels.confidence_set import ConfidenceSet
 
 
 @pytest.mark.parametrize(
-    "boundaries, values",
+    "boundaries, values, finite, empty",
     [
-        ([(-1, 1)], {-2: 1, 1: -1, 0: -1}),
-        ([(-np.inf, np.inf)], {-7: -1}),
-        ([(-np.inf, 0), (1, np.inf)], {0.5: 1, 12: -1}),
+        ([], {0: 1}, True, True),
+        ([(-1, 1)], {-2: 1, 1: -1, 0: -1}, True, False),
+        ([(-np.inf, np.inf)], {-7: -1}, False, False),
+        ([(-np.inf, 0), (1, 20)], {0.5: 1, 12: -1}, False, False),
     ],
 )
-def test_confidence_set_call(boundaries, values):
+def test_confidence_set_call(boundaries, values, finite, empty):
     confidence_set = ConfidenceSet(boundaries)
     for value, expected in values.items():
         assert confidence_set(value) == expected
+    assert confidence_set.is_finite() == finite
+    assert confidence_set.is_empty() == empty
