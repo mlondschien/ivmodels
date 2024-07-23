b76ad6e4e6e1e55bc268957e7c7f0eb9e715ebf8
Malte Londschien <malte@londschien.de>
2024-07-23 12:20:38 +0200

    New class: CoefficientTable.

--- ivmodels/summary.py
+++ ivmodels/summary.py
@@ -25,6 +25,22 @@ class Summary:
         Names of the features to be included in the summary. If not specified, all
         features will be included.
 
+    Attributes
+    ----------
+    coefficient_table_: CoefficientTable
+        Table containing the estimates, test statistics, p-values, and confidence sets
+        for each feature.
+    statistic_: float
+        Test statistic with null hypothesis that coefficients corresponding to the
+        endogenous regressors are jointly zero.
+    p_value_: float
+        P-value of the test statistic.
+    f_statistic_: float
+        F-statistic (or multivariate extension, see
+        :func:`~ivmodels.tests.rank.rank_test`) with null hypothesis that the
+        first-stage coefficient is of reduced rank.
+    f_p_value_: float
+        P-value of the F-statistic (or multivariate extension).
     """
 
     def __init__(self, kclass, test, alpha, feature_names=None):
@@ -75,7 +91,7 @@ class Summary:
             all_feature_names = ["intercept"] + all_feature_names
 
         if self.feature_names is None:
-            self.feature_names_ = all_feature_names
+            feature_names = all_feature_names
         else:
             extra_names = [n for n in self.feature_names if n not in all_feature_names]
             if len(extra_names) > 0:
@@ -83,7 +99,7 @@ class Summary:
                     f"Extra feature names {extra_names}. Available feature names are "
                     f"{all_feature_names}. Got feature names {self.feature_names}."
                 )
-            self.feature_names_ = self.feature_names
+            feature_names = self.feature_names
 
         import ivmodels.tests as tests  # avoid circular imports
 
@@ -117,10 +133,10 @@ class Summary:
 
         test_, inverse_test_ = _TESTS.get(str(self.test).lower())
 
-        self.estimates_ = list()
-        self.statistics_ = list()
-        self.p_values_ = list()
-        self.confidence_sets_ = list()
+        estimates = list()
+        statistics = list()
+        p_values = list()
+        confidence_sets = list()
 
         (X, Z, C), _ = self.kclass._X_Z_C(X, Z, C, predict=False)
         Z, X, y, _, C, _, _ = _check_inputs(Z, X, y, C=C)
@@ -128,7 +144,7 @@ class Summary:
         mx = len(self.kclass.endogenous_names_)
         fit_intercept = self.kclass.fit_intercept
 
-        for name in self.feature_names_:
+        for name in feature_names:
             idx = np.where(np.array(all_feature_names) == name)[0][0]
             mask = np.zeros(len(all_feature_names), dtype=bool)
             mask[idx] = True
@@ -137,9 +153,9 @@ class Summary:
                 mask = mask[1:]  # the first element corresponds to the intercept
 
             if name == "intercept":
-                self.estimates_.append(self.kclass.intercept_)
+                estimates.append(self.kclass.intercept_)
             else:
-                self.estimates_.append(self.kclass.coef_[idx - fit_intercept])
+                estimates.append(self.kclass.coef_[idx - fit_intercept])
 
             test_result = test_(
                 Z=Z,
@@ -152,8 +168,8 @@ class Summary:
                 fit_intercept=fit_intercept and name != "intercept",
                 **kwargs,
             )
-            self.statistics_.append(test_result[0])
-            self.p_values_.append(test_result[1])
+            statistics.append(test_result[0])
+            p_values.append(test_result[1])
 
             confidence_set = inverse_test_(
                 Z=Z,
@@ -169,7 +185,11 @@ class Summary:
             if isinstance(confidence_set, Quadric):
                 confidence_set = ConfidenceSet.from_quadric(confidence_set)
 
-            self.confidence_sets_.append(confidence_set)
+            confidence_sets.append(confidence_set)
+
+        self.coefficient_table_ = CoefficientTable(
+            feature_names, estimates, statistics, p_values, confidence_sets
+        )
 
         self.statistic_, self.p_value_ = test_(
             Z=Z, X=X, y=y, C=C, beta=np.zeros(X.shape[1]), fit_intercept=fit_intercept
@@ -181,39 +201,71 @@ class Summary:
         return self
 
     def __format__(self, format_spec: str) -> str:  # noqa D
-        if not hasattr(self, "estimates_"):
+        if not hasattr(self, "coefficient_table_"):
             return "Summary not fitted yet."
 
-        def format_p_value(x):
-            return f"{x:{format_spec}}" if np.isnan(x) or x > 1e-16 else "<1e-16"
+        string = f"Summary based on the {self.test} test.\n\n"
+        string += self.coefficient_table_.__format__(format_spec)
 
-        estimate_str = [f"{e:{format_spec}}" for e in self.estimates_]
-        statistics_str = [f"{s:{format_spec}}" for s in self.statistics_]
-        p_values_str = [format_p_value(p) for p in self.p_values_]
-        cis_str = [f"{cs:{format_spec}}" for cs in self.confidence_sets_]
+        string += f"""
 
-        names_len = max(len(name) for name in self.feature_names_)
+Endogenous model statistic: {self.statistic_:{format_spec}}, p-value: {_format_p_value(self.p_value_, format_spec)}
+(Multivariate) F-statistic: {self.f_statistic_:{format_spec}}, p-value: {_format_p_value(self.f_p_value_, format_spec)}"""
+        return string
+
+    def __str__(self):  # noqa D
+        return f"{self:.4g}"
+
+
+class CoefficientTable:
+    """
+    Table with estimates, statistics, p-values, and confidence sets for each feature.
+
+    Parameters
+    ----------
+    feature_names: list of str
+        Names of the features.
+    estimates: list of float
+        Estimates of the coefficients.
+    statistics: list of float
+        Test statistics.
+    p_values: list of float
+        P-values of the test statistics.
+    confidence_sets: list of ivmodels.confidence_set.ConfidenceSet
+        Confidence sets for the coefficients.
+    """
+
+    def __init__(self, feature_names, estimates, statistics, p_values, confidence_sets):
+        self.feature_names = feature_names
+        self.estimates = estimates
+        self.statistics = statistics
+        self.p_values = p_values
+        self.confidence_sets = confidence_sets
+
+    def __format__(self, format_spec: str) -> str:  # noqa D
+        estimate_str = [f"{e:{format_spec}}" for e in self.estimates]
+        statistics_str = [f"{s:{format_spec}}" for s in self.statistics]
+        p_values_str = [_format_p_value(p, format_spec) for p in self.p_values]
+        cis_str = [f"{cs:{format_spec}}" for cs in self.confidence_sets]
+
+        names_len = max(len(name) for name in self.feature_names)
         coefs_len = max(max(len(e) for e in estimate_str), len("estimate"))
         statistics_len = max(max(len(s) for s in statistics_str), len("statistic"))
         p_values_len = max(max(len(p) for p in p_values_str), len("p-value"))
         cis_len = max(max(len(ci) for ci in cis_str), len("conf. set"))
 
-        string = f"""Summary based on the {self.test} test.
-
-{'': <{names_len}}  {'estimate': >{coefs_len}}  {'statistic': >{statistics_len}}  {'p-value': >{p_values_len}}  {'conf. set': >{cis_len}}
-"""
-        # total_len = names_len + statistics_len + coefs_len + p_values_len + cis_len + 4
-        # string += "-" * total_len + "\n"
+        string = f"{'': <{names_len}}  {'estimate': >{coefs_len}}  {'statistic': >{statistics_len}}  {'p-value': >{p_values_len}}  {'conf. set': >{cis_len}}"
 
         for name, estimate, statistic, p_value, ci in zip(
-            self.feature_names_, estimate_str, statistics_str, p_values_str, cis_str
+            self.feature_names, estimate_str, statistics_str, p_values_str, cis_str
         ):
-            string += f"{name: <{names_len}}  {estimate: >{coefs_len}}  {statistic: >{statistics_len}}  {p_value: >{p_values_len}}  {ci: >{cis_len}}\n"
+            string += f"\n{name: <{names_len}}  {estimate: >{coefs_len}}  {statistic: >{statistics_len}}  {p_value: >{p_values_len}}  {ci: >{cis_len}}"
 
-        string += f"""
-Endogenous model statistic: {self.statistic_:{format_spec}}, p-value: {format_p_value(self.p_value_)}
-(Multivariate) F-statistic: {self.f_statistic_:{format_spec}}, p-value: {format_p_value(self.f_p_value_)}"""
         return string
 
     def __str__(self):  # noqa D
         return f"{self:.4g}"
+
+
+def _format_p_value(x, format_spec):
+    return f"{x:{format_spec}}" if np.isnan(x) or x > 1e-16 else "<1e-16"
