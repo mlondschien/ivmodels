c74b37883e32676c355cb71db0aabe97fc7e33c8
Malte Londschien <malte@londschien.de>
2025-04-30 15:17:50 +0200

    Add pre-test and docstring.

--- ivmodels/tests/residual_prediction.py
+++ ivmodels/tests/residual_prediction.py
@@ -15,11 +15,17 @@ def residual_prediction_test(
     kappa="tsls",
     fit_intercept=True,
     train_fraction=None,
-    clipping_quantile=0.8,
+    upper_clipping_quantile=0.9,
+    lower_clipping_value=0.1,
     seed=0,
 ):
     """
     Perform the residual prediction test for well-specification of the model.
+
+    This uses a nonlinear model to test
+
+    .. math::
+       H_0: \\exists \\beta_0 \\in \\mathbb{R}^p \\mathrm{ such that } \\mathbb{E}[y - X \\beta | Z] = 0.
 
     Parameters
     ----------
@@ -43,6 +49,18 @@ def residual_prediction_test(
         Fraction of data to use to train the nonlinear model (`estimator`). Must be
         between 0 and 1. The remaining data is used to compute the test statistic. If
         ``None``, 0.5 or ``e / log(n)`` is used, whichever is smaller.
+    upper_clipping_quantile: float, optional, default = 0.9
+        Asymptotic normality requires the nonlinear model's prediction not to put too
+        much weight in the tails. To avoid this, we clip its "test set" predictions by
+        a certail threshold in absolute value. The threshold is the
+        `upper_clipping_quantile` of predictions on the "train" data. Must be between 0
+        and 1.
+    lower_clipping_value: float, optional, default = 0.1
+        Asymptotic normality requires that the test statistics variance is not too
+        small. We do a pre-test for this. Clipping the predictions of the nonlinear
+        model to `lower_clipping_value` in absolute value, after rescaling, let's us
+        choose a reasonable threshold. Must be between 0 and 1. Set this to 0.0 to
+        disable the pre-test.
     seed: int, optional, default = 0
         Seed used to generate the random train / test split.
 
@@ -86,10 +104,11 @@ def residual_prediction_test(
             "nonlinear_model must have a `fit` and `predict` method. If you want to "
             "use a different model, please use the `sklearn` interface."
         )
-    # We split the data into 2 samples: _a and _b. A good choice for n_a is n / log(n).
-    # We fit a linear iv estimator beta_a : ya ~ Xa | Za and a nonlinear model
-    # (ya - Xa @ beta_a) ~ Za. Under H0: E[y - X beta_a | Z] = 0, there should be no
-    # signal in the residuals ya - Xa @ beta_a for the nonlinear model to learn.
+
+    # We split the data into 2 samples: _a and _b. A good choice for n_a is
+    # e * n / log(n). We fit a linear iv estimator beta_a : ya ~ Xa | Za and a nonlinear
+    # model (ya - Xa @ beta_a) ~ Za. Under H0: E[y - X beta_a | Z] = 0, there should be
+    # no signal in the residuals ya - Xa @ beta_a for the nonlinear model to learn.
     # We fit another linear iv estimator beta_b : yb ~ Xb | Zb. Let
     # wb := nonlinear_model.predict(Zb). Then, under H0, a properly rescaled version of
     # 1 / sqrt{nb} * wb.T @ (yb - Xb @ beta_b) / sigma_hat ~ N(0, 1)
@@ -109,8 +128,11 @@ def residual_prediction_test(
     residuals_b = yb - iv_model_b.predict(Xb, C=Cb).flatten()
 
     predictions_a = nonlinear_model.predict(X=ZCa).flatten()
-    clip = np.quantile(np.abs(predictions_a), clipping_quantile)
-    wb = np.clip(nonlinear_model.predict(X=ZCb) / clip, -1, 1)
+    upper_clipping_value = np.quantile(np.abs(predictions_a), upper_clipping_quantile)
+    wb = nonlinear_model.predict(X=ZCb).flatten()
+    wb = np.sign(wb) * np.clip(np.abs(wb), lower_clipping_value, upper_clipping_value)
+
+    gamma = 0.1 * np.mean(residuals_b**2) * lower_clipping_value**2
 
     XCb_proj = np.hstack([proj(np.hstack([Zb, Cb]), Xb), Cb])
     # pinv(X) = (X^T @ X)^(-1) @ X^T
@@ -120,6 +142,9 @@ def residual_prediction_test(
         )
         - np.mean(wb * residuals_b) ** 2
     )
+
+    if sigma_sq_hat < gamma:  # Pre-test for variance
+        return -np.inf, 1
 
     stat = wb.T @ residuals_b / np.sqrt(sigma_sq_hat) / np.sqrt(n)
     p_value = 1 - scipy.stats.norm.cdf(stat)
